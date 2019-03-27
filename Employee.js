59f8c852a25b894c65a365ce8e450cee62d7039f
Stanislao777 <e_vargas.v@outlook.com>
2019-03-27 11:06:33 -0400

    add change

--- Employee.js
+++ Employee.js
@@ -1,16 +1,41 @@
-let Employee = function(name, mount) {
-    this.name = name;
-    this.mount = mount;
-};
-
-Employee.prototype.getName = function() {
-    return this.name;
-};
-
-Employee.prototype.getMount = function() {
-    return this.mount;
-};
-
-if (typeof window === "undefined") {
-    module.exports = Employee;
+class Employees{
+    constructor(ci, name, mount){
+        this.ci=ci;
+        this.name=name;
+        this.mount=mount;
+    }
+    calculatorBalance(){
+        throw new Error("This type is not recognized");
+    }
 }
+
+
+class TimeComplete extends Employees{
+    constructor(ci, name, mount){
+        super(ci, name, mount);
+    }
+
+    calculatorBalance(){
+        return this.mount;
+    }
+}
+/*
+class TimeParcial extends Employees{
+    constructor(ci, name, mount, horas){
+        super(ci, name, mount);
+        this.horas=horas;
+    }
+
+    calculatorBalance(){
+        return this.mount*this.horas;
+    }
+}
+
+
+class Comision extends Employees{
+    constructor(ci, name, mount, porcentageCommission){
+        super(ci, name, mount)r;
+    }
+}*/
+
+
--- test/employee_test.js
+++ test/employee_test.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 var should = require('chai').should();
 
 import Employee from '../Employee.js';
-import TicketGenerator from '../TicketGenerator.js';
+//import TicketGenerator from '../TicketGenerator.js';
 
 describe('EmployeeClass', function() {
 
@@ -13,17 +13,17 @@ describe('EmployeeClass', function() {
         ticketGenerator = new TicketGenerator();
     });
 
-    it('Deberia retornar el nombre del empleado', function() {
+    /*it('Deberia retornar el nombre del empleado', function() {
         let employee = new Employee('Jorge', 1000);
         expect(employee.getName()).equal('Jorge');
-        employee = new Employee('Estanis', 1000);
-        expect(employee.getName()).equal('Estanis');
+        employee = new Employee('Pedro', 1000);
+        expect(employee.getName()).equal('Pedro');
     });
 
     it('Deberia retornar el monto del empleado', function() {
         let employee = new Employee('Jorge', 1000);
         expect(employee.getMount()).equal(1000);
-        employee = new Employee('Estanis', 2000);
+        employee = new Employee('Juaquin', 2000);
         expect(employee.getMount()).equal(2000);
     });
 
@@ -36,4 +36,9 @@ describe('EmployeeClass', function() {
         expect(ticketGenerator.getSalary(employee)).equal(30000);
     });
 
+    it('Deberia retornar el sueldo del empleado por horas', function() {
+        let employee = new Employee('Jorge', 100);
+        //expect(ticketGenerator.getSalary(employee)).equal(1200);
+    });
+*/
 });
