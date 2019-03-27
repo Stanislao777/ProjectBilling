e6eaabfa1a8a08b81aec4ae6a6f20341deb55b31
Stanislao777 <e_vargas.v@outlook.com>
2019-03-27 11:37:12 -0400

    Refactorizacion ScheduleTable

--- ScheduleTable.js
+++ ScheduleTable.js
@@ -1,13 +1,16 @@
 class ScheduleTable {
 
     constructor(schedulesTimes) {
-        this.schedulesTimes = schedulesTimes;
+        this.schedulesTimes = [];
+        schedulesTimes.forEach(schedule => {
+            this.schedulesTimes.push([new Date(schedule[1]), new Date(schedule[0])]);
+        });
     }
 
     getHours() {
         let response = 0;
-        this.schedulesTimes.forEach(function(schedule) {
-            response += Number.parseInt((new Date(schedule[1]) - new Date(schedule[0])) / 3600000);
+        this.schedulesTimes.forEach(schedule => {
+            response += Number.parseInt((schedule[0] - schedule[1]) / 3600000);
         });
         return response;
     }
