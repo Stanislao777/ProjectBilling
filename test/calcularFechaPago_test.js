dc8b44bc9885a09e382d8207f0dfe47d0b7c847e
Stanislao777 <e_vargas.v@outlook.com>
2019-04-09 07:15:28 -0400

    pruebas para fecha pago por comision

--- test/calcularFechaPago_test.js
+++ test/calcularFechaPago_test.js
@@ -1,6 +1,8 @@
 let expect = require('chai').expect;
 
-import CalculadoraFechaPago from '../CalculadoraFechaPago.js';
+import Horarios from '../Horarios';
+import  CalculadoraFechaPago from "../CalculadoraFechaPago";
+
 
 describe('calcularFechaPago',function () {
     it('Deberia retornar que es posible poder pagar a un empleado por hora en viernes', function () {
@@ -8,6 +10,14 @@ describe('calcularFechaPago',function () {
         let calculadoraFechaPago = new CalculadoraFechaPago('Horas',fecha);
 
         expect(calculadoraFechaPago.estaDisponibleParaPagar()).equal(true);
+    });
+
+    it('Deberia retornar que es posible pagar a un empleado por comision', function () {
+        let fechaInicial = new Date("2019-04-01 08:00:00");
+        let fechaFinal =new Date("2019-04-12 18:00:00");
+        let calcularFechaPago = new CalculadoraFechaPago('Comision', fechaInicial, fechaFinal);
+
+        expect(calcularFechaPago.estaDisponibleParaPagar()).equal(true);
     });
 
 });
