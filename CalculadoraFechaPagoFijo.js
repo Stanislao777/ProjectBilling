d33b6be6eb245b92c277c28baa2bf760e7ef9668
Stanislao777 <e_vargas.v@outlook.com>
2019-04-10 17:26:30 -0400

    refactorizando y simplificando algunas líneas

--- CalculadoraFechaPagoFijo.js
+++ CalculadoraFechaPagoFijo.js
@@ -3,13 +3,18 @@ class CalculadoraFechaPagoFijo {
         this.fechaInicial = fechaInicial;
     }
 
+    reducirDIa(fecha) {
+        fecha.setDate(fecha.getDate() - 1);
+        return fecha;
+    }
+
     inicioDePago() {
         let ultimaFechaDelMes = new Date(this.fechaInicial.getFullYear(), this.fechaInicial.getMonth() + 1, 0);
         if(ultimaFechaDelMes.getDay() === 0) {
-            ultimaFechaDelMes.setDate(ultimaFechaDelMes.getDate() - 1);
+            ultimaFechaDelMes = this.reducirDIa(ultimaFechaDelMes);
         }
         if(ultimaFechaDelMes.getDay() === 6) {
-            ultimaFechaDelMes.setDate(ultimaFechaDelMes.getDate() - 1);
+            ultimaFechaDelMes = this.reducirDIa(ultimaFechaDelMes);
         }
         return ultimaFechaDelMes.getDate() + '-' + (ultimaFechaDelMes.getMonth() + 1) + '-' + ultimaFechaDelMes.getFullYear();
     }
