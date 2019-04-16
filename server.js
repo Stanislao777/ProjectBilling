e32a942f40ef273304c8be43d85bde3c3c584273
Stanislao777 <e_vargas.v@outlook.com>
2019-04-16 10:18:44 -0400

    actualizando bd

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ let db = new sqlite3.Database('./dataBase.db');
 
 const createTable = () => {
     console.log("creando base de datos paara tabla empleado");
-    db.run("CREATE TABLE IF NOT EXISTS empleado(id INTEGER PRIMARY KEY AUTOINCREMENT, ci INTEGER,name TEXT, calcularSalario TEXT)",  insertData);
+    db.run("CREATE TABLE IF NOT EXISTS empleado(" +
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        "ci INTEGER,name TEXT, " +
+        "calcularSalario TEXT)",
+        insertData);
 }
 
 const insertData = () =>{
-    console.log("Insertar datos")
+    console.log("Insertando datos")
     db.run('INSERT INTO empleado (ci, name, calcularSalario) VALUES (?,?,?)', ["123","felipe", 1232]);
 }
 
---
+++ test/empleado_db_test.js
@@ -0,0 +1,12 @@
+let expect = require('chai').expect;
+
+import Empleado from '../Empleado';
+
+describe('Base de datos', function () {
+
+    it('Si creamos un empleado deberia insertar en la BD correctamente', function () {
+
+    });
+
+});
+
